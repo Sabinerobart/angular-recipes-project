5d3d4e361e071c79d2fcad14220bb1aa0ee39849
Sabine Robart <sabine.robart@protonmail.com>
2020-04-02 14:59:04 +0200

    Get recipes from the recipes service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,24 @@
+import { Recipe } from './recipe.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      "Blueberry pie",
+      "Who doesn't love blueberry pies ?",
+      "https://images.unsplash.com/photo-1464305795204-6f5bbfc7fb81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+    ),
+    new Recipe(
+      "Blueberry muffins",
+      "Delicious blueberry muffins",
+      "https://images.unsplash.com/photo-1525124541374-b7eaf79d0dbf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+    )
+  ];
+
+  getRecipes() {
+    // Empty .slice() returns a copy of the original array
+    return this.recipes.slice();
+  }
+}
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,29 +1,23 @@
 import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: "app-recipes-list",
   templateUrl: "./recipes-list.component.html",
   styleUrls: ["./recipes-list.component.css"]
 })
+
 export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      "Blueberry pie",
-      "Who doesn't love blueberry pies ?",
-      "https://images.unsplash.com/photo-1464305795204-6f5bbfc7fb81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-    ),
-    new Recipe(
-      "Blueberry muffins",
-      "Delicious blueberry muffins",
-      "https://images.unsplash.com/photo-1525124541374-b7eaf79d0dbf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-    )
-  ];
 
-  constructor() {}
+  recipes: Recipe[];
 
-  ngOnInit() {}
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
