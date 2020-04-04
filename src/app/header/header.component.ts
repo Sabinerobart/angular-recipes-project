30fce8d2bb05021f768069559af70ed9d4604a38
Sabine Robart <sabine.robart@protonmail.com>
2020-04-04 11:52:02 +0200

    Add router + update navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,9 @@
-import { Component, Output, EventEmitter } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
-  collapsed = true;
-  @Output() navClick = new EventEmitter<string>();
 
-  onSelect(name: string) {
-    this.navClick.emit(name);
-  }
 }
