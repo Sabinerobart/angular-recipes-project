ff42fdc3f1a2b1762bc231cdc64222154b93bc74
Sabine Robart <sabine.robart@protonmail.com>
2020-04-08 13:21:05 +0200

    Add editing recipes (existing ones + adding new ones)

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -57,7 +57,7 @@ export class RecipeEditComponent implements OnInit {
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName, Validators.required),
       'imagePath': new FormControl(recipeImg, Validators.required),
-      'imageDescr': new FormControl(recipeDescr, Validators.required),
+      'description': new FormControl(recipeDescr, Validators.required),
       // formArrayName
       'ingredients': recipeIngredients
     })
@@ -67,12 +67,22 @@ export class RecipeEditComponent implements OnInit {
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
         'name': new FormControl(null, Validators.required),
-        'amount': new FormControl(null, [Validators.required, Validators.pattern('/^[1-9]+[0-9]*$/')])
+        'amount': new FormControl(null, [Validators.required, Validators.pattern('^[1-9]+[0-9]*$')])
       })
     )
   }
 
   onSubmit() {
-    console.log(this.recipeForm)
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['ingredients']
+    // );
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value)
+    }
   }
 }
