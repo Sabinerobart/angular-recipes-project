5968fece9fe497da2b5fd16c3f963b94db3914b4
Sabine Robart <sabine.robart@protonmail.com>
2020-04-09 15:08:46 +0200

    Transform recipes fetched from db with an empty array of ingredients when there are none to fix a bug

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,20 +10,22 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      "Blueberry pie",
-      "Who doesn't love blueberry pies ?",
-      "https://images.unsplash.com/photo-1464305795204-6f5bbfc7fb81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-      [new Ingredient('Blueberry', 30), new Ingredient('Sugar', 100)]
-    ),
-    new Recipe(
-      "Blueberry muffins",
-      "Delicious blueberry muffins",
-      "https://images.unsplash.com/photo-1525124541374-b7eaf79d0dbf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-      [new Ingredient('Blueberry', 30), new Ingredient('Sugar', 70), new Ingredient('Flour', 100)]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "Blueberry pie",
+  //     "Who doesn't love blueberry pies ?",
+  //     "https://images.unsplash.com/photo-1464305795204-6f5bbfc7fb81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+  //     [new Ingredient('Blueberry', 30), new Ingredient('Sugar', 100)]
+  //   ),
+  //   new Recipe(
+  //     "Blueberry muffins",
+  //     "Delicious blueberry muffins",
+  //     "https://images.unsplash.com/photo-1525124541374-b7eaf79d0dbf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  //     [new Ingredient('Blueberry', 30), new Ingredient('Sugar', 70), new Ingredient('Flour', 100)]
+  //   )
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) { }
 
