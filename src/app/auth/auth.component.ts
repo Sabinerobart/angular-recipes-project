d6dfd21a8c9a6b3d120e0af295639070588ae5af
Sabine Robart <sabine.robart@protonmail.com>
2020-04-10 12:11:50 +0200

    Add error handling to the login form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseDate } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -30,22 +31,26 @@ export class AuthComponent implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObs: Observable<AuthResponseDate>;
+
     if (this.isLoginMode) {
-      //...
+      authObs = this.authService.login(email, password);
     } else {
       this.isLoading = true;
-      this.authService.signup(email, password).subscribe(res => console.log(res), errorMessage => {
-        console.log(errorMessage);
-        this.error = errorMessage;
-        this.isLoading = false;
-        // Without the pipe in the auth.service :
-        // switch (err.error.error.message) {
-        //   case 'EMAIL_EXISTS':
-        //     this.error = 'This email already exists !';
-        // }
-      })
+      authObs = this.authService.signup(email, password);
     }
+    authObs.subscribe(res => console.log(res), errorMessage => {
+      // Without the pipe in the auth.service :
+      // switch (err.error.error.message) {
+      //   case 'EMAIL_EXISTS':
+      //     this.error = 'This email already exists !';
+      // }
+      console.log(errorMessage);
+      this.error = errorMessage;
+      this.isLoading = false;
+    })
     form.reset();
+    this.error = '';
   }
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseDate {
+export interface AuthResponseDate {
   kind: string,
   idToken: string,
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  registered?: boolean // ? to make it optionnal (required for login but not for signup)
 }
 
 @Injectable({
@@ -25,20 +26,33 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true
-    }).pipe(catchError(err => {
-      let errorMessage = 'An unknown error occured !';
-      if (!err.error || !err.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (err.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email already exists !';
-      }
-      return throwError(errorMessage)
-    }));
+    }).pipe(catchError(this.handleError));
   }
 
-  signin() {
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseDate>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.API_KEY}`, {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }).pipe(catchError(this.handleError))
+  }
 
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured !';
+    if (!err.error || !err.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (err.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists !';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Email or password are incorrect';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Email or password are incorrect';
+        break;
+    }
+    return throwError(errorMessage)
   }
 }
