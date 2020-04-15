70bda783ea72151c7e77c976bf2c14f326ec3344
Sabine Robart <sabine.robart@protonmail.com>
2020-04-15 09:49:06 +0200

    Update the actions constant names to avoid name clashes in case we use the same types of actions in different state management (dispatches to all reducers)

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,12 @@
 import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
-export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
-export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+export const ADD_INGREDIENT = '[Shopping List] Add_ingredient';
+export const ADD_INGREDIENTS = '[Shopping List] Add_ingredients';
+export const UPDATE_INGREDIENT = '[Shopping List] Update_ingredient';
+export const DELETE_INGREDIENT = '[Shopping List] Delete_ingredient';
+export const START_EDIT = '[Shopping List] Start_edit';
+export const STOP_EDIT = '[Shopping List] Stop_edit';
 
 
 export class AddIngredient implements Action {
